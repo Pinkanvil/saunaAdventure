8e1a106ef6d36218ab34ee6907397f2f45cc1da0
Pinkanvil <antti.kaulio2@gmail.com>
2023-11-23 11:04:07 +0200

    Debug lisätty healthia varten

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class Health : MonoBehaviour
 {
     public int maxHealth = 3;
     public int currentHealth;
-    
+
+    // Debug dataa varten
+    public GUIStyle myStyle;
+
     // Start is called before the first frame update
     void Start()
     {
+        myStyle.fontSize = 18;
+        myStyle.normal.textColor = Color.white;
+
+
         currentHealth = maxHealth;
     }
 
@@ -27,5 +36,10 @@ public class Health : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnGUI()
+    {
+        // Debug data
+        GUI.Label(new Rect(10, 10, 100, 20), "Health : " + currentHealth, myStyle);
     }
 }
